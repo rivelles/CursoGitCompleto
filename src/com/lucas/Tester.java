15a06ddb82100d6c5c3c05515a3c5690462b2929
LucasRivelles <rivelleslucas@gmail.com>
2018-09-30 18:53:39 -0300

    Criação do aplicativo

---
+++ src/com/lucas/Tester.java
@@ -0,0 +1,33 @@
+package com.lucas;
+
+import java.io.IOException;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPatch;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class Tester {
+	public String doPatch(String uri) throws ClientProtocolException, IOException {
+		HttpPatch patch = new HttpPatch(uri);
+		CloseableHttpClient http = HttpClientBuilder.create().build();
+		
+		String jsonPayload = "";
+		patch.setEntity(new StringEntity(jsonPayload, ContentType.APPLICATION_JSON));
+		
+		HttpResponse response = http.execute(patch);
+
+		String out = response.getEntity().toString();
+		
+		return out;
+	}
+	
+	public static void main(String[] args) throws ClientProtocolException, IOException {
+		String out = new Tester().doPatch("http://www.google.com.br");
+		System.out.println(out);
+	}
+}
